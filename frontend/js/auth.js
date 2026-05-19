772727bb61aa0d047ea422af556ed96bbf8a5d37
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-05-19 09:29:02 -0300

    Login screen working normally

---
+++ frontend/js/auth.js
@@ -0,0 +1,36 @@
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const loginValue = document.getElementById('login').value;
+    const passwordValue = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMsg');
+
+    const loginData = {
+        login: loginValue,
+        password: passwordValue
+    };
+
+    try{
+        const response = await fetch('http://localhost:8080/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        });
+
+        if(response.ok){
+            const data = await response.json();
+            const token = data.token;
+            localStorage.setItem('token', token);
+            window.location.href = '/pages/home.html';
+        }
+        else{
+            errorMessage.innerText = 'Usuário ou senha inválidos';
+        }
+    }
+    catch(error){
+        console.error('Erro ao conectar com o servidor:', error);
+        errorMessage.innerText = 'Erro ao conectar com o servidor. Tente mais tarde.';
+    }
+})
