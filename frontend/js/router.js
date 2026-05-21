5e96f7fa584e3a00d8800089cdba4117ce6fc990
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-05-21 14:11:09 -0300

    created authGuard to prevent access without a valid token

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -1,16 +1,20 @@
 function loadScreen(screenPath) {
+
     const template = document.getElementById('card');
 
     fetch(`/pages/${screenPath}.html`)
         .then(response => {
-            if (!response.ok) throw new Error();
-            return response.text();
+            if (!response.ok) {
+                throw new Error();
+            }
+            else {
+                return response.text();
+            }
         })
         .then(html => {
             template.innerHTML = html;
         })
         .catch(error => {
-            template.innerHTML = `<h3>Error</h3><p>${error.message}</p>`;
+            template.innerHTML = `<h3>Error: </h3><p>${error.message}</p>`;
         });
-
 }
