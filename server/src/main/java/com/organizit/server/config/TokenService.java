260261383889c525be5fc7b0da362b341e7b306b
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-05-26 15:57:01 -0300

    Changing the token expiration time to 20 minutes

--- server/src/main/java/com/organizit/server/config/TokenService.java
+++ server/src/main/java/com/organizit/server/config/TokenService.java
@@ -50,7 +50,7 @@ public class TokenService {
 	}
 	
 	private Instant generateExpirationDate() {
-		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+		return LocalDateTime.now().plusMinutes(20).toInstant(ZoneOffset.of("-03:00"));
 	}
 	
 }
