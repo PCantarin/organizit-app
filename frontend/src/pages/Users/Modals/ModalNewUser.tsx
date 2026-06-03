6285d04b1a13592a7ff9e0856ebb08598ee4c771
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-06-03 14:18:59 -0300

    Create de skeleton of the new user modal component

--- frontend/src/pages/Users/Modals/ModalNewUser.tsx
+++ frontend/src/pages/Users/Modals/ModalNewUser.tsx
@@ -1,29 +1,72 @@
-import { Dialog, DialogActions, DialogContent, Divider } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  FormControlLabel,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
 import ModalHeader from "../../../components/ModalHeader";
 import FormTextField from "../../../components/Inputs/FormTextField";
 import SimpleButton from "../../../components/Buttons/SimpleButton";
-import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded';
+import PersonAddAltRoundedIcon from "@mui/icons-material/PersonAddAltRounded";
 import SecretTextField from "../../../components/Inputs/SecretTextField";
+import CustomRadio from "../../../components/Inputs/CustomRadio";
+import PageDivider from "../../../components/PageDivider";
 
 type ModalNewUserProps = {
-    onClose: () => void;
-    open: boolean;
-    onSubmit: (event: React.SubmitEvent) => void;
-}
+  onClose: () => void;
+  open: boolean;
+  onSubmit: (event: React.SubmitEvent) => void;
+};
 
-function ModalNewUser({onClose, open, onSubmit}: ModalNewUserProps) {
+function ModalNewUser({ onClose, open, onSubmit }: ModalNewUserProps) {
   return (
     <Dialog onClose={onClose} open={open}>
-      <ModalHeader text="Adicionar novo usuário" icon={PersonAddAltRoundedIcon} />
+      <ModalHeader
+        text="Adicionar novo usuário"
+        icon={PersonAddAltRoundedIcon}
+      />
       <DialogContent>
-        <form onSubmit={onSubmit}>
+        <form
+          onSubmit={onSubmit}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "500px",
+            margin: "0 auto",
+            padding: "20px",
+          }}
+        >
           <FormTextField name="name" label="Nome" required={true} />
           <FormTextField name="username" label="Usuário" required={true} />
 
           <SecretTextField label="Senha" name="password" required={true} />
-          <SecretTextField label="Repita a senha" name="password" required={true} />
+          <SecretTextField
+            label="Repita a senha"
+            name="password"
+            required={true}
+          />
 
-          <Divider sx={{ mt: 1 }} />
+          <Box sx={{ mt: 4 }}>
+            <Typography>Selecione o nível de permissão:</Typography>
+
+            <RadioGroup sx={{ ml: 3 }} defaultValue={"user"}>
+              <FormControlLabel
+                value="user"
+                control={<CustomRadio />}
+                label="User"
+              />
+              <FormControlLabel
+                value="admin"
+                control={<CustomRadio />}
+                label="Admin"
+              />
+            </RadioGroup>
+          </Box>
+
+          <PageDivider marginBottom={2} marginTop={10}/>
           <DialogActions>
             <SimpleButton type="submit" text="Cadastrar" />
           </DialogActions>
