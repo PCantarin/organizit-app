b76bc9759f11f711bbab544e8be9b992733a2836
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-06-03 14:18:11 -0300

    Add optional props on the page divider component

--- frontend/src/components/PageDivider.tsx
+++ frontend/src/components/PageDivider.tsx
@@ -1,10 +1,15 @@
 import { Divider } from "@mui/material";
 
+type PageDividerProps = {
+  height?: number; 
+  marginBottom?: number;
+  marginTop?: number;
+}
 
-function PageDivider(){
+function PageDivider({ height = 1, marginBottom = 20, marginTop }: PageDividerProps){
     return(
         <Divider
-        sx={{ marginBottom: "20px", border: "solid 1px", borderRadius: "10px" }}
+        sx={{ marginBottom: `${marginBottom}px`, marginTop:`${marginTop}px`, border: `solid ${height}px`, borderRadius: "10px" }}
       />
     )
 }
